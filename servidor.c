edaf2504383eca1d6beb442f94694beaf78ff5fa
maK1naR1a <155095615+maK1naR1a@users.noreply.github.com>
2024-12-08 04:02:22 +0100

    V0

--- servidor.c
+++ servidor.c
@@ -1,103 +1,109 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <pwd.h>
+#include <signal.h>
 
-#define PUERTO 81023
+#define PUERTO 8080
 
-int main (int argc, char* argv)
-{
-    // Creacion del socket
-    int s_TCP;                        // Descriptor de conexion del socket
-    ls_tcp = socket (AF_INET, SOCK_STREAM, 0);
-    if (ls_tcp == -1)
-    {
-        perror(argv[0]);
-        fprintf(stderr, "%s: unable to create socket TCP\n", argv[0]);
+void serverTCP(int s, struct sockaddr_in clientaddr_in);
+
+int main(int argc, char* argv[]) {
+    int s_TCP, ls_tcp;
+    ls_tcp = socket(AF_INET, SOCK_STREAM, 0);
+    if (ls_tcp == -1) {
+        perror("Error creando socket TCP");
         exit(1);
     }
-    // Socket created
 
-    // Bind del socket
     struct sockaddr_in myaddr_in;
-    memset ((char *)&myaddr_in, 0 , sizeof(struct sockaddr_in));
+    memset((char *)&myaddr_in, 0, sizeof(struct sockaddr_in));
 
     myaddr_in.sin_family = AF_INET;
-    myaddr_in.sin_addr.s_addr = htonl(INADDR_ANY); // 0.0.0.0 - Cualquiera de mis ip's
+    myaddr_in.sin_addr.s_addr = htonl(INADDR_ANY);
     myaddr_in.sin_port = htons(PUERTO);
 
-    if (bind(ls_TCP, (const struct sockaddr *) &myaddr_in, sizeof(struct sockaddr_in)) == -1)
-    {
-        perror(argv[0]);
-        fprintf(stderr, "%s: unable to bind address TCP\n", argv[0]);
+    if (bind(ls_tcp, (const struct sockaddr *)&myaddr_in, sizeof(struct sockaddr_in)) == -1) {
+        perror("Error en bind");
         exit(1);
     }
-    // Socket binded
 
-    // Create listen queue
-    if (listen(ls_tcp, 5) == -1)
-    {
-        perror(argv[0]);
-        fprintf(stderr, "%s: unable to listen on socket TCP\n", argv[0]);
+    if (listen(ls_tcp, 5) == -1) {
+        perror("Error en listen");
         exit(1);
     }
-    // Socket ready to listen
 
-    // Make the proccess a Daemon
     setpgrp();
-    switch(fork())
-    {
-        case -1:  
-        // Unable to fork
-            perror(argv[0]);
-            fprintf(stderr, "%s: unable to fork Daemon \n", argv[0]);
-            exit(1);
+    if (fork() == 0) {
+        close(0);
+        close(2);
 
-        case 0 : 
-        // Child
-            close(stdin);
-            close(stderr);
-            if (sigaction(SIGCHILD, &sa, NULL) == -1)
-            {
-                perror("sigaction(SIGCHILD)");
-                fprintf(stderr, "%s: unable to register SIGCHILD signal \n", argv[0]);
-                exit(1);
+        struct sigaction sa;
+        sa.sa_handler = SIG_IGN;
+        sa.sa_flags = 0;
+        sigaction(SIGCHLD, &sa, NULL);
+
+        struct sockaddr_in clientaddr_in;
+        socklen_t addrlen = sizeof(clientaddr_in);
+
+        while (1) {
+            s_TCP = accept(ls_tcp, (struct sockaddr *)&clientaddr_in, &addrlen);
+            if (s_TCP == -1) continue;
+
+            if (fork() == 0) {
+                close(ls_tcp);
+                serverTCP(s_TCP, clientaddr_in);
+                exit(0);
             }
+            close(s_TCP);
+        }
+    }
 
-            //Use to unbind terminal death with the program
-            vec.sa_handler = (void *)finalizar;
-            vec.sa_flags = 0;
-            if (sigaction(SIGTERM, &vec, (struct sigaction *) 0) == -1)
-            {
-                perror("sigaction(SIGTERM)");
-                fprintf(stderr, "%s: unable to register SIGTERM signal \n", argv[0]);
-                exit(1);
-            }
-            while(!FIN)
-            {
-                s_TCP = accept(ls_tcp, (struct sockaddr *)&clientaddr_in, &addrlen); // Handler de socket ya conectado a un cliente especifico
-                if (s_TCP == -1) exit(1);   
-                switch (fork())
-                {
-                case -1:
-                // Can't fork
-                    exit(1);
+    return 0;
+}
 
-                case 0:
-                // Niece created for each connection
-                    close(ls_tcp); //Close the listen socket inherited from the daemon
-                    serverTCP(s_TCP, clientaddr_in);
-                    exit(0);
-                
-                default:
-                // Daemon
-                    close(s_tcp); // The daemon closes the new accept socket when 
-                                  // a new child is created. Prevents of running out of FD space.
+void serverTCP(int s, struct sockaddr_in clientaddr_in) {
+    char buffer[1024];
+    ssize_t len = read(s, buffer, sizeof(buffer) - 1);
+    if (len <= 0) {
+        close(s);
+        return;
+    }
+
+    buffer[len] = '\0';  // Termina el string
+    char response[4096] = {0};
+
+    if (strlen(buffer) > 1) {  // Usuario especificado
+        buffer[strcspn(buffer, "\r\n")] = '\0';  // Quita nueva línea
+        struct passwd *pw = getpwnam(buffer);
+        if (pw) {
+            snprintf(response, sizeof(response),
+                     "Nombre: %s\nUID: %d\nGID: %d\nDirectorio: %s\nShell: %s\n",
+                     pw->pw_name, pw->pw_uid, pw->pw_gid, pw->pw_dir, pw->pw_shell);
+        } else {
+            snprintf(response, sizeof(response), "Usuario '%s' no encontrado.\n", buffer);
+        }
+    } else {  // Lista de todos los usuarios
+        FILE *fp = fopen("/etc/passwd", "r");
+        if (!fp) {
+            snprintf(response, sizeof(response), "Error abriendo /etc/passwd.\n");
+        } else {
+            char line[256];
+            while (fgets(line, sizeof(line), fp)) {
+                char *user = strtok(line, ":");
+                if (user) {
+                    strncat(response, user, sizeof(response) - strlen(response) - 1);
+                    strncat(response, "\n", sizeof(response) - strlen(response) - 1);
                 }
             }
-    }// Proccess made a Daemon and running serverTCP
- }
+            fclose(fp);
+        }
+    }
 
- void serverTCP(int s, struct sockaddr_in clientaddr_in)
- {
-    // TODO: Aqui se configura lo que hara el servidor  
- }
+    write(s, response, strlen(response));
+    close(s);
+}
